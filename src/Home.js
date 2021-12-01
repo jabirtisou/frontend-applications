9a1bd2a7269281c78ffd63c2dd859c395c982cfb
Jabir Tisoudali <jabirtisou@hotmail.com>
2021-12-01 21:11:49 +0100

    homepage

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
-    return (
-        <div>
-            <h2>Home</h2>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Big mac price index</h1>
+      <p>
+        De Big Mac-index is een grappige berekeningswijze van koopkracht
+        gebaseerd op de prijs van een Big Mac in een bepaald land. De index is
+        bedacht door het Britse weekblad The Economist in 1986 en maakte een
+        vergelijking tussen de verschillende valuta's ten opzichte van de
+        Amerikaanse dollar. Door de koopkracht van de dollar te vergelijken met
+        die van een andere munt, kan men zien hoeveel dollars die munt
+        'eigenlijk' waard is en of de wisselkoers te hoog of te laag is. De
+        koopkrachtvergelijking doet men in dit geval heel eenvoudig, door te
+        kijken naar de prijs van een Big Mac, die overal ter wereld wordt
+        gefabriceerd en verkocht. Zo kan men een beoordeling maken van de
+        geldende wisselkoersen.
+      </p>
+      <iframe
+        src='https://giphy.com/embed/RLWxuPR1Zrm9KRIEkC'
+        width='480'
+        title='burger'
+        height='352'
+        frameBorder='0'
+        class='giphy-embed'
+        allowFullScreen
+      ></iframe>
+      <iframe
+        src='https://giphy.com/embed/cl72XB7TqPdjq7uQ3S'
+        width='450'
+        height='480'
+        title='ham'
+        frameBorder='0'
+        class='giphy-embed'
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
